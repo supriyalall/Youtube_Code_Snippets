521cb3614619307ffdaccbfe37ce571ac05b1fd1
supriyalall <lallsupriya365@gmail.com>
2022-10-01 22:41:49 -0400

    removing unsolved portion

--- Dart/Flutter_Python_Series/Post_Request/Unsolved/backend_code_snippet/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify, request
-import json
-
-# creating the instance of our flask application
-app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- Dart/Flutter_Python_Series/Post_Request/Unsolved/frontend_code_snippet/android/app/src/main/kotlin/com/example/frontend_code_snippet/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.frontend_code_snippet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
